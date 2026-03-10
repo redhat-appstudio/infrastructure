0618599bd0339dd47b2764fea6a987b1d13becc9
Gal Levi <glevi@redhat.com>
2026-03-10 18:32:15 +0200

    PoC: use etcd-defrag CLI to prevent etcd defrag abuse

--- maintenance/etcd/build-deps/deps.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-
-package deps
-
-import (
-	_ "go.etcd.io/etcd/etcdctl/v3"
-)
-
--- maintenance/etcd/defrag.sh
+++ maintenance/etcd/defrag.sh
@@ -2,41 +2,13 @@
 
 set -x
 
-diskUsageThreshold=${diskUsageThreshold:-60}
-fragmentationThreshold=${fragmentationThreshold:-30}
-reclaimSpaceThreshold=${reclaimSpaceThreshold:-1024}
+DEFRAG_RULE=${DEFRAG_RULE:-"dbQuotaUsage > 0.6 && ((dbSize - dbSizeInUse) * 100 / dbSize > 30 || dbSize - dbSizeInUse > 1024 * 1024 * 1024)"}
 
-first_endpoint=$(echo $ETCDCTL_ENDPOINTS | cut -d',' -f1)
-
-echo $first_endpoint
-
-rev=$(ETCDCTL_ENDPOINTS="${first_endpoint}" etcdctl endpoint status --write-out="json" | egrep -o '"revision":[0-9]*' | egrep -o '[0-9]*')
-etcdctl compact --command-timeout 60s --physical $rev
-
-diff=$(ETCDCTL_ENDPOINTS="${first_endpoint}" etcdctl endpoint status --write-out=json | jq -r '.[] | .Status.dbSize - .Status.dbSizeInUse')
-ondisk=$(ETCDCTL_ENDPOINTS="${first_endpoint}" etcdctl endpoint status --write-out=json | jq -r '.[] | .Status.dbSize')
-
-# Calculate fragmented percentage
-fragmentedPercentage=$(( ${diff} * 100 / ${ondisk} ))
-echo "Fragmented Percentage: ${fragmentedPercentage}%"
-
-# Calculate fragmented space in megabytes
-fragmentedSpace=$(( ${diff} / 1024 / 1024 ))
-echo "Fragmented Space: ${fragmentedSpace} MB"
-
-diskUsage=$((${ondisk} * 100 / (8 * 1024 * 1024 * 1024)))
-echo "Disk Usage: ${diskUsage}"
-
-# Perform defragmentation if needed
-if [ "$diskUsage" -lt "$diskUsageThreshold" ]; then
-    echo "Disk usage is below threshold, no defragmentation needed."
-    exit 0
-fi
-
-if [ "$fragmentedPercentage" -ge "$fragmentationThreshold" ] || [ "$fragmentedSpace" -ge "$reclaimSpaceThreshold" ]; then
-    echo "Fragmentation is above threshold, performing defragmentation..."
-    etcdctl defrag --command-timeout 60s
-else
-    echo "No defragmentation needed."
-    exit 0
-fi
+etcd-defrag \
+  --endpoints "${ETCDCTL_ENDPOINTS}" \
+  --compaction \
+  --cluster \
+  --continue-on-error \
+  --wait-between-defrags "${WAIT_BETWEEN_DEFRAGS:-30s}" \
+  --defrag-rule "${DEFRAG_RULE}" \
+  "$@"
